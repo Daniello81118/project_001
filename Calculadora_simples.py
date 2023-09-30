97a50c54e66f23d6fd4dd9fdb6207d39a273fd92
Daniello81118 <145640812+Daniello81118@users.noreply.github.com>
2023-09-30 01:59:09 -0300

    Calculadora_simples.py

--- Calculadora_simples.py
+++ Calculadora_simples.py
@@ -1,33 +1,59 @@
-# funções
-def adicao(a, b):
-  return a + b
-def subtracao(a, b):
-  return a - b
-def multiplicacao(a, b):
-  return a * b
-def divisao(a, b):
-  if b != 0:
-    return a / b
-  else:
-    return "Não é possivel dividir por zero"
+import math
 
-print("Selecione a operação: ")
-print("1. Adição")
-print("2. Subtração")
-print("3. Multiplicação")
-print("4. Divisão")
+def operacoes_trigonometricas():
+    print("Operações Trigonométricas:")
+    print("1. Seno")
+    print("2. Cosseno")
+    print("3. Tangente")
+    
+    escolha = input("Digite 1/2/3: ")
+    angulo = float(input("Digite o ângulo em graus: "))
 
-escolha = input("Digite a opção: ")
-num1 = float(input("Digite o primeiro número: "))
-num2 = float(input("Digite o segundo número: "))
+    if escolha == '1':
+        resultado = math.sin(math.radians(angulo))
+        print(f"Seno({angulo}°) = {resultado}")
+    elif escolha == '2':
+        resultado = math.cos(math.radians(angulo))
+        print(f"Cosseno({angulo}°) = {resultado}")
+    elif escolha == '3':
+        resultado = math.tan(math.radians(angulo))
+        print(f"Tangente({angulo}°) = {resultado}")
+    else:
+        print("Escolha inválida")
 
-if ecolha == '1':
-  print("Resultado:", adicao(num1, num2))
-elif escolha == '2':
-  print("Resultado:", subtracao(num1, num2))
-elif escolha == '3':
-  print("Resultado:", multiplicacao(num1, num2))
-elif escolha == '4':
-  print("Resultado:", divisao(num1, num2))
-else:
-  print("Escolha inválida")
+def operacoes_raiz_e_potencia():
+    print("Operações de Raiz Quadrada e Potência:")
+    print("1. Raiz Quadrada")
+    print("2. Potência")
+    
+    escolha = input("Digite 1/2: ")
+
+    if escolha == '1':
+        numero = float(input("Digite um número: "))
+        resultado = math.sqrt(numero)
+        print(f"Raiz Quadrada de {numero} = {resultado}")
+    elif escolha == '2':
+        base = float(input("Digite a base: "))
+        expoente = float(input("Digite o expoente: "))
+        resultado = math.pow(base, expoente)
+        print(f"{base}^{expoente} = {resultado}")
+    else:
+        print("Escolha inválida")
+
+while True:
+    print("\nSelecione a operação:")
+    print("1. Operações Trigonométricas")
+    print("2. Operações de Raiz Quadrada e Potência")
+    print("3. Sair")
+
+    escolha = input("Digite 1/2/3: ")
+
+    if escolha == '1':
+        operacoes_trigonometricas()
+    elif escolha == '2':
+        operacoes_raiz_e_potencia()
+    elif escolha == '3':
+        print("Encerrando a calculadora.")
+        break
+    else:
+        print("Escolha inválida")
